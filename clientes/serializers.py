258915b03d121106cb49fbd3fdcf7ef0dab65b6d
Mel-Rocha <rochamel73@gmail.com>
2024-04-01 15:28:04 -0300

    validation basic

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
 from clientes.models import Cliente
 
+
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O CPF deve ter 11 digitos")
+        return cpf
+
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Apenas letras")
+        return nome
+
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve ter 9 digitos")
+        return rg
+
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("O celular dever ter no mínimo 11 digitos")
+        return celular
