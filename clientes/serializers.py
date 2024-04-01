866964730fc08fa9645c3ffa2fd25c1ff68cf394
Mel-Rocha <rochamel73@gmail.com>
2024-04-01 20:08:51 -0300

    populate database

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,28 +1,19 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 digitos")
-        return cpf
-
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("Apenas letras")
-        return nome
-
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O RG deve ter 9 digitos")
-        return rg
-
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError("O celular dever ter no mínimo 11 digitos")
-        return celular
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"CPF inválido"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celulcar': "Formato inválido"})
+        return data
